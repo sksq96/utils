774155c20bace9a0e6d09d6a4fd548b3bc342cf3
Shubham Chandel <shubhamchandel@nyu.edu>
2020-10-19 16:17:09 -0700

    Update argparser.py

--- argparser.py
+++ argparser.py
@@ -1,11 +1,11 @@
+import argparse
 
-
-parser = ArgumentParser()
-parser = ArgumentParser()
+parser = argparse.ArgumentParser(description='Short sample app')
 parser.add_argument("--model_dir", type=str, required=True)
 parser.add_argument("--code_before", type=str, required=True)
 parser.add_argument("--depth", type=int, default=10)
 parser.add_argument("--breadth", type=int, default=10)
 parser.add_argument("--min_tokens", type=int, default=2)
 parser.add_argument("--language", type=str, required=True)
+parser.add_argument('--train', action="store_true", default=False)
 args = parser.parse_args()
