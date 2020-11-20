adc120a18a0ebaa67dc3513da4fdf5ce87921107
Shubham Chandel <shubhamchandel@nyu.edu>
2020-11-19 20:10:52 -0800

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,6 @@
+import logging
+logger = logging.getLogger(__name__)
+# Setup logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
