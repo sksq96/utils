d90c5732807d9293c3819396012429f85beb1c49
Shubham Chandel <shubhamchandel@nyu.edu>
2021-03-15 18:10:30 -0700

    Update schema.py

--- schema.py
+++ schema.py
@@ -6,3 +6,28 @@ def schema(d, idx=0):
         print("\t"*idx, k)
         if isinstance(v, dict):
             schema(v, idx+1)
+            
+
+            
+"""
+d = {
+    "Where":2,
+    "to?": {
+        "Help": 1,
+        "Inside!": {
+            "Even": 1,
+            "More inside!!": 2
+        }
+    }
+}
+
+schema(d)
+>>> 
+ Where
+ to?
+   Help
+   Inside!
+     Even
+     More inside!!
+
+"""
